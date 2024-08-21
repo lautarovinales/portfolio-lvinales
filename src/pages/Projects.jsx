4ffb37742ddeff6f13519a2a7b0e9818d848f5b6
lautarovinales <lautarovinales14@gmail.com>
2024-08-21 18:01:02 -0300

    nuevo estilo xd

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,7 +18,35 @@ function About() {
         <SubTitle text={"Estos son algunos de mis proyectos realizados a traves de mi camino como desarrollador web full stack. Debajo encontrarás proyectos tantos de Back-end como de Front-end cada uno con detalles importantes sobre los mismos."}></SubTitle>
         <Divider></Divider>
         <div className="project-container">
-          <img src="" alt="" />
+          <img src="/proyecto-zapa.jpg" alt="" />
+          <div className="body-project">
+            <div className="tags-project">
+              <div className="tag-project">SQL</div>
+              <div className="tag-project">NODE</div>
+              <div className="tag-project">ESPRESS</div>
+              <div className="tag-project">BACKEND</div>
+              <div className="tag-project">FRONTEND</div>
+            </div>
+            <div className="title-project">E-commerce "Sneaker Spot"</div>
+            <div className="link-project"><a href="https://github.com/lautarovinales/grupo_3_SneakerSpot">Repositorio</a></div>
+            </div>
+        </div>
+        <div className="project-container">
+          <img src="/proyecto-zapa.jpg" alt="" />
+          <div className="body-project">
+            <div className="tags-project">
+              <div className="tag-project">SQL</div>
+              <div className="tag-project">NODE</div>
+              <div className="tag-project">ESPRESS</div>
+              <div className="tag-project">BACKEND</div>
+              <div className="tag-project">FRONTEND</div>
+            </div>
+            <div className="title-project">E-commerce "Sneaker Spot"</div>
+            <div className="link-project"><a href="https://github.com/lautarovinales/grupo_3_SneakerSpot">Repositorio</a></div>
+            </div>
+        </div>
+        <div className="project-container">
+          <img src="/proyecto-zapa.jpg" alt="" />
           <div className="body-project">
             <div className="tags-project">
               <div className="tag-project">SQL</div>
